b2d3532b667175690784d33af36cbcab26b73ac1
Karlos <karlos.hbraga@gmail.com>
2019-10-23 22:25:06 -0300

    Login funcionando.

--- multimeios/multimeios/settings.py
+++ multimeios/multimeios/settings.py
@@ -137,3 +137,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'djreservas', 'statics'),
 ]
+
+LOGIN_REDIRECT_URL = '/djreservas/'
--- multimeios/multimeios/urls.py
+++ multimeios/multimeios/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('djreservas/', include('djreservas.urls')),
     path('admin/', admin.site.urls),
+    path('djreservas/', include('djreservas.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
