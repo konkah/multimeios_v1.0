ea04a9ce2f49bd438b012d8d45ce94b0fc76d427
Karlos <karlos.hbraga@gmail.com>
2019-10-29 01:34:53 -0300

    limpada index para adicionar proximos eventos das salas

--- multimeios/multimeios/settings.py
+++ multimeios/multimeios/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 ugettext = lambda s: s
 LANGUAGES = (
     ('pt_BR', ugettext('Português Brasileiro')),
